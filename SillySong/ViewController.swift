dde500b0372e55fecdc4d5f5925f2c9eb4d35c1e
nsutanto <nicholas.sutanto@cummins.com>
2017-07-19 23:26:11 -0400

    Init (#1)

---
+++ SillySong/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SillySong
+//
+//  Created by Nicholas Sutanto on 7/19/17.
+//  Copyright © 2017 Nicholas Sutanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
